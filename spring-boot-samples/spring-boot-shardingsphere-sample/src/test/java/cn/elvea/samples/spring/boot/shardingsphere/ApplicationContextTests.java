38f679ff490cb7894d952a847c74cf6da546add1
Elvea Huang <elvea.huang@gmail.com>
2020-10-27 18:04:04 +0800

    初始ShardingShere示例

---
+++ spring-boot-samples/spring-boot-shardingsphere-sample/src/test/java/cn/elvea/samples/spring/boot/shardingsphere/ApplicationContextTests.java
@@ -0,0 +1,16 @@
+package cn.elvea.samples.spring.boot.shardingsphere;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class ApplicationContextTests {
+
+    @Test
+    public void testCrud() {
+    }
+
+}
