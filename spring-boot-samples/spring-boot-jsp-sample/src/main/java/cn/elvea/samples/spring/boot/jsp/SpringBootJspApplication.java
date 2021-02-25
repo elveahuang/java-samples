92e42a0165a0e9c15ef1d2c93717441ef3d1b8e9
Elvea Huang <elvea.huang@gmail.com>
2021-02-25 16:42:02 +0800

    新增JSP示例，目前貌似Spring Boot 2.4.x有兼容性问题，跟自定义LocaleResolver有关

---
+++ spring-boot-samples/spring-boot-jsp-sample/src/main/java/cn/elvea/samples/spring/boot/jsp/SpringBootJspApplication.java
@@ -0,0 +1,18 @@
+package cn.elvea.samples.spring.boot.jsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBootJspApplication
+ *
+ * @author elvea
+ */
+@SpringBootApplication
+public class SpringBootJspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJspApplication.class, args);
+    }
+
+}
---
+++ spring-boot-samples/spring-boot-jsp-sample/src/main/java/cn/elvea/samples/spring/boot/jsp/config/WebMvcConfig.java
@@ -0,0 +1,31 @@
+package cn.elvea.samples.spring.boot.jsp.config;
+
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/views/", ".jsp");
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() {
+        return factory -> factory.setRegisterDefaultServlet(true);
+    }
+
+}
